e532f5bc1ea9579ef1ea6669eb8f55adf2f38a30
Johnny Herrera <jmjaramillo95@gmail.com>
2025-07-03 22:12:34 -0500

    Agregar archivos de proyecto.

---
+++ list/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace list
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Object> lista = new List<Object>();
+            List<String> lista2 = new List<String>();
+            lista.Add(32);
+            lista.Add('a');
+            lista.Add(true);
+            //lista.Remove(32);
+            lista.ForEach(item =>
+            {
+                Console.WriteLine(item);
+            });
+
+            for (int i = 0; i < lista.Count; i++)
+            {
+                Console.WriteLine(lista[i]);
+            }
+
+            Console.WriteLine(lista.Count());
+        }
+    }
+}
