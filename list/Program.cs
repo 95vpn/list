fe47f640d108c33cb4c6dd83fc68a77bf1f5b5ed
Johnny Herrera <jmjaramillo95@gmail.com>
2025-07-04 23:55:34 -0500

    mestodos-list

--- list/Program.cs
+++ list/Program.cs
@@ -13,20 +13,56 @@ namespace list
             List<Object> lista = new List<Object>();
             List<String> lista2 = new List<String>();
             lista.Add(32);
+            lista.Add("Alex");
             lista.Add('a');
             lista.Add(true);
+            lista.Add("Alex");
+            lista.Add(false);
+            lista.Add("Alex");
             //lista.Remove(32);
+            /*
             lista.ForEach(item =>
             {
                 Console.WriteLine(item);
             });
+            */
+            lista.Insert(2, "Joel");
+            //lista.Clear();
+            lista.Reverse();
+            Console.WriteLine(lista.Contains(329));
+            //Console.WriteLine(lista.LastIndexOf("Alex", 3));
+            //lista.LastIndexOf("Alex")
+            //Console.WriteLine(lista.IndexOf(32,0,4));
+            foreach(var item in lista)
+            {
+                Console.WriteLine(item);
+            }
 
+            /*
             for (int i = 0; i < lista.Count; i++)
             {
                 Console.WriteLine(lista[i]);
             }
+            
+            */
+            /*
+            int j = 0;
+
+            for (; ; )
+            {
+                if(j < lista.Count)
+                {
+                    Console.WriteLine(lista[j]);
+                    j++;
+                }
+                else
+                {
+                    break;
+                }
+            }
 
             Console.WriteLine(lista.Count());
+            */
         }
     }
 }
